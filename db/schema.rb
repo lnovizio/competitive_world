cba83ab998c7afb718ffa4789b21b3e77005f256
lnovizio <lnovizio@gmail.com>
2020-09-17 20:25:20 +0200

    contenuti

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200916133532) do
+ActiveRecord::Schema.define(version: 20200917181028) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string "titolo"
+    t.text "descrizione"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
